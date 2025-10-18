b2c8a32b9f1cc400a7966afa1b016587328e6d62
mobashshirhasan <mobashshir12@gmail.com>
2025-10-18 16:03:41 +0530

    Square of side 'N'

---
+++ pattern_questions/square_of_n_side.py
@@ -0,0 +1,28 @@
+'''
+Square of side 'N'
+Problem Description: You are given an integer n. Your task is to return a square pattern of 
+size n x n made up of the character '*', represented as a list of strings.
+
+'''
+
+
+def generate_square(n):
+    """
+    Function to return a square pattern of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The size of the square.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the square.
+    """
+
+    lst = []
+    for i in range(n):
+        str = ""
+        for j in range(n):
+            str = str + "*"
+        lst.append(str)
+    
+    return lst
+
---
+++ practice.py
@@ -0,0 +1,15 @@
+''' 
+    Prime Number
+'''
+num = int(input("Enter a number :"))
+
+count = 0 
+
+for i in range(1, num+1):
+    if num % i == 0:
+        count = count+1
+
+if count == 2:
+    print("number is prime ")
+else:
+    print("numer is not prime")
